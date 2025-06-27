f4c9abd9dc1b0af3577b0f4c3dfc5f7b12a3919e
Fatima Slim <fatimaslim157@gmail.com>
2025-06-27 12:29:59 -0400

    Initial commit with third-party dependencies

---
+++ build/third-party/cnpy/CMakeFiles/example1.dir/DependInfo.cmake
@@ -0,0 +1,22 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/ahmad/ahmad/projects/New_Setup/DeepSDF/third-party/cnpy/example1.cpp" "/home/ahmad/ahmad/projects/New_Setup/DeepSDF/build/third-party/cnpy/CMakeFiles/example1.dir/example1.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../third-party/eigen3.3.7"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/home/ahmad/ahmad/projects/New_Setup/DeepSDF/build/third-party/cnpy/CMakeFiles/cnpy.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
